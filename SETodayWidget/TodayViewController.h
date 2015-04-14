0b6689c9848b1553679cfb56741acdba2ddd622c
A UiBrian <anluan@binarybuzzsoftware.com>
2015-04-14 11:23:11 -0400

    check if sharedApplication is available, use it

---
+++ SETodayWidget/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  SETodayWidget
+//
+//  Created by Anluan O'Brien on 4/14/15.
+//  Copyright (c) 2015 Anluan O'Brien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
